7ba1b5bab299abc22d5e4aeff6ac3ba53a4a2cfb
BigBlackBob-93 <101857203+BigBlackBob-93@users.noreply.github.com>
2023-02-19 03:26:38 +0700

    init

---
+++ project/app/__init__.py
@@ -0,0 +1,7 @@
+from PyQt6.QtWidgets import QApplication
+import sys
+
+app = QApplication(sys.argv)
+from app import objects
+
+sys.exit(app.exec())
---
+++ project/app/objects.py
@@ -0,0 +1,8 @@
+from PyQt6.QtWidgets import QMainWindow
+
+# window
+window = QMainWindow()
+window.setWindowTitle("Simulation: Flight")
+window.setGeometry(500, 300, 500, 500)
+
+window.show()
